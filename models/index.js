c94c40791b5ef155de9bde99c2b0ced044164917
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-05-30 13:25:21 -0400

    add views to better separate functions

--- models/index.js
+++ models/index.js
@@ -1,36 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment');
-
 
 User.hasMany(Post, {
-    foreignKey: 'userId',
-    
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
-   
+  foreignKey: 'user_id',
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'userId',
-  
-});
-
-// Comment.belongsTo(User, {
-//     foreignKey: 'userId',
-  
-// });
-
-Post.hasMany(Comment, {
-    foreignKey: 'postId',
-   
-});
-
-// Comment.belongsTo(Post, {
-//     foreignKey: 'postId',
-  
-// });
-
-module.exports = { User, Post, Comment};
+module.exports = { User, Post };
